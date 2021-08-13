cf3c76465a114844cff8484dc8d7e66ce192a765
Andres <andres.konrad@hotmail.com>
2021-08-13 06:47:14 +0200

    Update relay links

--- frontend/src/app/random-relay/random-relay.component.ts
+++ frontend/src/app/random-relay/random-relay.component.ts
@@ -11,10 +11,18 @@ export class RandomRelayComponent implements OnInit {
 
   private links = [
     "https://töffligpbollhof.ch/ah-mann/",
-    "https://töffligpbollhof.ch/zoom/",
-    "https://töffligpbollhof.ch/gif/",
-    "https://de.crazygames.com/spiele/wheelie-bike",
-    "https://der-schrauberling.de/lachgas-mofa/",
+"https://töffligpbollhof.ch/zoom/",
+"https://töffligpbollhof.ch/gif/",
+"https://de.crazygames.com/spiele/wheelie-bike",
+"https://fooby.ch/de/rezepte/14203/gschwellti?startAuto1=0",
+"https://www.tutti.ch/de/vi/aargau/fahrzeuge/motorraeder/mocambo-original-renntoeffli/46850221",
+"https://töffligpbollhof.ch/win/",
+"https://töffligpbollhof.ch/dieser-herr",
+"https://töffligpbollhof.ch/kiss/",
+"https://töffligpbollhof.ch/gschwelltigeil/",
+"https://toeffligpbollhof.ch/shot/",
+"https://toeffligpbollhof.ch/jenny/",
+"https://toeffligpbollhof.ch/burger/"
   ];
 
   ngOnInit() {
